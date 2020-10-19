1719a236cc894c8cd3a82443edaf38897eeb9a78
yoooov <yoooov@protonmail.com>
2020-10-19 19:13:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 feedie = {
     'bot_owner': ['meigrafd'],
-    'cmd_prefix': '@',
+    'cmd_prefix': '!',
     #'shorten_service': 'v.gd', #requires ssl
     'shorten_service': 'tinyurl.com',
 }
@@ -33,49 +33,6 @@ feeds = [{
         'channel': '#FEEDs',
         'channel_key': '',
         'enabled': True,
-    },
-    'chillmo': {
-        'url': 'http://chillmo.com/feed/',
-        'color': 'orange',
-        'channel': '#FEEDs',
-        'channel_key': '',
-        'enabled': True,
-    },
-    'hwluxx': {
-        'url': 'http://www.hardwareluxx.de/index.php/rss/feed/3-hardwareluxx-rss-feed.html',
-        'color': 'teal',
-        'channel': '#FEEDs',
-        'channel_key': '',
-        'enabled': True,
-    },
-    'ht4u': {
-        'url': 'http://ht4u.net/feeds/news.xml',
-        'color': 'purple',
-        'channel': '#FEEDs',
-        'channel_key': '',
-        'enabled': True,
-    },
-    'pcgames': {
-        'url': 'http://www.pcgames.de/feed.cfm?menu_alias=home',
-        'color': 'pink',
-        'channel': '#FEEDs',
-        'channel_key': '',
-        'enabled': True,
-    },
-    'pcgames_hw': {
-        'url': 'http://www.pcgameshardware.de/feed.cfm',
-        'color': 'red',
-        'channel': '#FEEDs',
-        'channel_key': '',
-        'enabled': True,
-    },
-    'forum': {
-        'url': 'http://www.forum-raspberrypi.de/syndication2.php?limit=10',
-        'color': 'red',
-        'channel': '#Raspberry-Pi',
-        'channel_key': '',
-        'enabled': True,
-        'refresh_delay': 15.0,
     },
 }]
 
